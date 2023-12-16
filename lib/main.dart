19af0d0fa0a421152a6b06b405430bd8663f2d89
Miguel31218 <miguel.chguillermo@gmail.com>
2023-12-16 09:25:58 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:firebaseapp/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "MyFirebaseApp",
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
